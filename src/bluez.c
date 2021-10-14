83f9c1b3c274d57b16846f57eb893bd7c60f0382
Ravi Chandra Padmala <ravi@asymptotic.com>
2021-10-15 01:13:17 +0530

    Apply clang-format on bluez.c

--- src/bluez.c
+++ src/bluez.c
@@ -1,15 +1,14 @@
-#include <stdint.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include "log.h"
-#include <stdint.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/l2cap.h>
 #include <errno.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static void setopts(int s) {
-  struct l2cap_options opts = { 0 };
+  struct l2cap_options opts = {0};
   int err;
   unsigned int size = sizeof(opts);
   err = getsockopt(s, SOL_L2CAP, L2CAP_OPTIONS, &opts, &size);
@@ -18,7 +17,7 @@ static void setopts(int s) {
     opts.omtu = opts.imtu = 167;
     log_info("Setting sockopts\n");
     err = setsockopt(s, SOL_L2CAP, L2CAP_OPTIONS, &opts, size);
-    if (!err) 
+    if (!err)
       log_info("Set sockopts\n");
     else {
       log_info("Unable to set sockopts. %s (%d)\n", strerror(errno), errno);
@@ -31,20 +30,22 @@ static void setopts(int s) {
 void l2cap_connect(char *bd_addr_raw, uint16_t psm) {
   int s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_L2CAP), status;
   if (s == -1) {
-    log_info("L2CAP: Could not create a socket %s:%u. %s (%d)\n", bd_addr_raw, psm, strerror(errno), errno);
+    log_info("L2CAP: Could not create a socket %s:%u. %s (%d)\n", bd_addr_raw,
+             psm, strerror(errno), errno);
   } else
     log_info("L2CAP: Created socket %s:%u\n", bd_addr_raw, psm);
 
-  struct sockaddr_l2 addr = { 0 };
+  struct sockaddr_l2 addr = {0};
   addr.l2_family = AF_BLUETOOTH;
   addr.l2_psm = htobs(psm);
-  // See https://elixir.bootlin.com/linux/latest/source/net/bluetooth/l2cap_sock.c#L238
+  // See
+  // https://elixir.bootlin.com/linux/latest/source/net/bluetooth/l2cap_sock.c#L238
   addr.l2_bdaddr_type = BDADDR_LE_PUBLIC;
 
   char bd_addr[30];
   strncpy(bd_addr, bd_addr_raw, 30);
 
-  for (size_t i=0; i < strnlen(bd_addr, 30); i++) {
+  for (size_t i = 0; i < strnlen(bd_addr, 30); i++) {
     if (bd_addr[i] == '_') {
       bd_addr[i] = ':';
     }
@@ -53,12 +54,13 @@ void l2cap_connect(char *bd_addr_raw, uint16_t psm) {
   str2ba(bd_addr, &addr.l2_bdaddr);
 
   setopts(s);
-  status = connect(s, (struct sockaddr *) &addr, sizeof(addr));
+  status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
   if (status == 0)
     log_info("L2CAP: Connected to %s:%u\n", bd_addr, psm);
   else
-    log_info("L2CAP: Could not connect to %s:%u. Error %d. %d (%s)\n", bd_addr, psm, status, errno, strerror(errno));
+    log_info("L2CAP: Could not connect to %s:%u. Error %d. %d (%s)\n", bd_addr,
+             psm, status, errno, strerror(errno));
 
   close(s);
 }
