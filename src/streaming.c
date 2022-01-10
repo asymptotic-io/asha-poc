dac12f0ab08dd619e385c9cfc11f8050de2341a4
Arun Raghavan <arun@asymptotic.io>
2022-01-10 10:08:48 -0500

    wip: Switch from write() to send() and always pack SDU length
    This is wrong, because the stack should pack SDU len.

--- src/streaming.c
+++ src/streaming.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -24,7 +25,7 @@ bool stream_init(char *bd_addr_raw, struct ha_device *device) {
   device->sequence_counter = 0;
   device->socket = l2cap_connect(bd_addr_raw, device->le_psm);
   device->source = open("/tmp/sample.g722", O_RDONLY);
-  device->firstrun = 0;
+  device->firstrun = 1;
 
   // 1 byte for seq_counter and 2 for sdulen in the first sdu
   device->sdulen = 161;
@@ -45,17 +46,17 @@ bool stream_init(char *bd_addr_raw, struct ha_device *device) {
 void stream_act(struct ha_device *device) {
   ssize_t bytes_processed = 0;
 
-  for (uint8_t i = 0; i < 200; i++) {
+  for (uint8_t i = 0; i < 2000; i++) {
     if (device->firstrun) {
       memcpy(device->buffer, &device->sdulen, 2);
       memcpy(device->buffer + 2, &device->sequence_counter, 1);
       memcpy(device->buffer + 3, device->sample, sizeof(device->sample));
       device->firstrun = 1;
-      bytes_processed = write(device->socket, device->buffer, device->sdulen + 2);
+      bytes_processed = send(device->socket, device->buffer, device->sdulen + 2, 0);
     } else {
       memcpy(device->buffer, &device->sequence_counter, 1);
       memcpy(device->buffer + 1, device->sample, sizeof(device->sample));
-      bytes_processed = write(device->socket, device->buffer, device->sdulen);
+      bytes_processed = send(device->socket, device->buffer, device->sdulen, 0);
     }
 
     if (bytes_processed < 0) {
