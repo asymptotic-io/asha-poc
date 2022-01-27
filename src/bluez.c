b6c989e842e4e6659f69691c6210e004556f6d8b
Ravi Chandra Padmala <ravi@asymptotic.io>
2022-01-27 16:42:04 +0530

    Clean up bluez.c
    Aside from some minor refactor, there are some functional changes as
    well in the ordering of the set opts, bind and connect calls

--- src/bluez.c
+++ src/bluez.c
@@ -8,26 +8,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-static void setopts(int s) {
+// Set based on the spec: https://source.android.com/devices/bluetooth/asha
+#define MTU 167
+
+static void set_mtu(int s, int mtu) {
   struct l2cap_options opts = {0};
-  int err;
+  int err = 0;
   unsigned int size = sizeof(opts.imtu);
 
-  opts.omtu = opts.imtu = 202;
+  opts.omtu = opts.imtu = mtu;
   log_info("Setting sockopts\n");
+
   err = setsockopt(s, SOL_BLUETOOTH, BT_RCVMTU, &opts.imtu, size);
-  if (!err)
-    log_info("Set recv mtu\n");
-  else {
+  if (err) {
     log_info("Unable to set recv mtu. %s (%d)\n", strerror(errno), errno);
+  } else {
+    log_info("Set recv mtu\n");
   }
+
   err = setsockopt(s, SOL_BLUETOOTH, BT_SNDMTU, &opts.imtu, size);
-  if (!err)
-    log_info("Set send mtu\n");
-  else {
+  if (err) {
     log_info("Unable to set send mtu. %s (%d)\n", strerror(errno), errno);
+  } else {
+    log_info("Set send mtu\n");
   }
 }
+
+static void strsub(char *str, char c, char replacement, int len) {
+  for (size_t i = 0; i < len; i++) {
+    if (str[i] == c) {
+      str[i] = replacement;
+    }
+  }
+}
+
+#define ADDR_LENGTH 30
 
 int l2cap_connect(char *bd_addr_raw, uint16_t psm) {
   int s = socket(PF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
@@ -42,33 +57,31 @@ int l2cap_connect(char *bd_addr_raw, uint16_t psm) {
   log_info("L2CAP: Created socket %s:%u\n", bd_addr_raw, psm);
 
   struct sockaddr_l2 addr = {0};
+  char bd_addr[ADDR_LENGTH];
+  strncpy(bd_addr, bd_addr_raw, ADDR_LENGTH);
+
   addr.l2_family = AF_BLUETOOTH;
-  // See
-  // https://elixir.bootlin.com/linux/latest/source/net/bluetooth/l2cap_sock.c#L238
   addr.l2_bdaddr_type = BDADDR_LE_PUBLIC;
+  addr.l2_psm = htobs(psm);
+
+  strsub(bd_addr, '_', ':', ADDR_LENGTH);
+
+  str2ba(bd_addr, &addr.l2_bdaddr);
+
+  set_mtu(s, MTU);
+
+  // Currently need to bind before connect to workaround an issue where the
+  // addr type is incorrectly set otherwise
 
   status = bind(s, (struct sockaddr *)&addr, sizeof(addr));
+
   if (status < 0) {
     log_info("L2CAP: Could not bind %s\n", strerror(errno));
     return -1;
   }
 
-  log_info("L2CAP socket bound\n");
+  log_info("L2CAP: socket bound\n");
 
-  addr.l2_psm = htobs(psm);
-
-  char bd_addr[30];
-  strncpy(bd_addr, bd_addr_raw, 30);
-
-  for (size_t i = 0; i < strnlen(bd_addr, 30); i++) {
-    if (bd_addr[i] == '_') {
-      bd_addr[i] = ':';
-    }
-  }
-
-  str2ba(bd_addr, &addr.l2_bdaddr);
-
-  setopts(s);
   status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
   if (status == 0) {
